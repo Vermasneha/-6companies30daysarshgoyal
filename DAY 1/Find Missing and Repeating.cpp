0de92ed6dd65ed4b10071c9ae9dffb6157fff2b7
Sneha kumari <90461178+Vermasneha@users.noreply.github.com>
2024-01-07 22:48:07 +0530

    Update Find Missing and Repeating.cpp

--- DAY 1/Find Missing and Repeating.cpp
+++ DAY 1/Find Missing and Repeating.cpp
@@ -1,1 +1,39 @@
+class Solution{
+public:
+    long long  dupe(vector<int>arr){
+        sort(arr.begin(),arr.end());
+        for(int i=0;i<arr.size()-1;i++){
+            if(arr[i]==arr[i+1]){
+                return arr[i];
+            }
+        }
+    }
+    
+    long long  sum(vector<int>arr){
+        long long int summ=0;
+        for(int i=0;i<arr.size();i++){
+            summ+=arr[i];
+        }
+        return summ;
+    }
+    
+    vector<long> findTwoElement(vector<int> arr, int n) {
+        // code here
+        long long int y=n;
+        long long  B=dupe(arr);
+        long long int Sum=sum(arr);
+        Sum-=B;
+         long long int expectedSum=(y*(y+1))/2;
+
+ 
+
+        long long int A= expectedSum-Sum;
+        // cout<<expectedSum<<endl<<Sum <<endl;
+        vector<long>ans;
+        ans.push_back(B);
+        ans.push_back(A);
+        return ans;
+    }
+};
+
 
