119f5b17ec50da20a416ee306563c74ea15178e7
Sneha kumari <90461178+Vermasneha@users.noreply.github.com>
2024-01-08 18:21:48 +0530

    Update Find the Winner.cpp

--- Company 2/Find the Winner.cpp
+++ Company 2/Find the Winner.cpp
@@ -1,1 +1,16 @@
+class Solution {
+public:
+    int findTheWinner(int n, int k) {
+        vector<int> v(n);
+        for(int i=0;i<n;i++){
+            v[i] = i+1;
+        }
 
+        int pos = 0;
+        while(v.size()!=1){
+            pos = (pos+k-1)%v.size();
+            v.erase(v.begin()+pos);
+        }
+        return v[0];
+    }
+};
