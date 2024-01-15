121ea07abc7ece96577220338c183adca2cf7023
Sneha kumari <90461178+Vermasneha@users.noreply.github.com>
2024-01-15 21:50:16 +0530

    Update Number-of-ways-to-reach-a-position-after-exactly-k-steps.cpp

--- Company 1/Number-of-ways-to-reach-a-position-after-exactly-k-steps.cpp
+++ Company 1/Number-of-ways-to-reach-a-position-after-exactly-k-steps.cpp
@@ -1,1 +1,19 @@
+class Solution {
+public:
+    int solve(int startPos, int endPos, int k){
+        //Base Condn
+        if(k==0){
+            if(startPos==endPos) return 1;  //If we reach the endpos in k steps
+            else return 0;
+        }
+        
+        int a = solve(startPos+1, endPos, k-1);   //Step Forward
+        int b = solve(startPos-1, endPos, k-1);   //Step Backward
+        
+        return (a+b);   //Return total ways
+    }
 
+    int numberOfWays(int startPos, int endPos, int k) {
+        return solve(startPos, endPos, k);
+    }
+};
