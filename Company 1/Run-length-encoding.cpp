66eafab196704638b5f21d319996084d06d3b7c0
Sneha kumari <90461178+Vermasneha@users.noreply.github.com>
2024-01-15 21:49:07 +0530

    Update Run-length-encoding.cpp

--- Company 1/Run-length-encoding.cpp
+++ Company 1/Run-length-encoding.cpp
@@ -1,1 +1,19 @@
+string encode(string src) {
+    int start = 0;
+    int end = 0;
+    string encodedString;
 
+    while (end < src.length()) {
+        start = end;
+
+        // Find the length of the current run of identical characters
+        while (end < src.length() && src[end] == src[start]) {
+            end++;
+        }
+
+        // Append the character and its count to the encoded string
+        encodedString += src[start] + to_string(end - start);
+    }
+
+    return encodedString;
+}
