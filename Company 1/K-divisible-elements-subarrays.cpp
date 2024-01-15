0ce12927bb41ce16de2f1333065a0f993c3fdc66
Sneha kumari <90461178+Vermasneha@users.noreply.github.com>
2024-01-15 21:54:14 +0530

    Update K-divisible-elements-subarrays.cpp

--- Company 1/K-divisible-elements-subarrays.cpp
+++ Company 1/K-divisible-elements-subarrays.cpp
@@ -1,1 +1,26 @@
+class Solution {
+public:
+    int countDistinct(vector<int>& nums, int k, int p) {
+        
+        int n = nums.size();
+        unordered_set<string>hashing;
 
+        for(int i = 0; i < n; i++) {
+            string hash = "";
+            int count = 0;
+            for(int j = i; j < n; j++) {
+                hash += (nums[j] + '0');
+                if(nums[j] % p == 0) {
+                    count++;
+                }
+                if(count > k) {
+                    break;
+                }
+                hashing.insert(hash);
+            }
+        }
+
+        return hashing.size();
+
+    }
+};
