e589a96820a0edffd4f92bf10e7f2892ec3aaa88
Sneha kumari <90461178+Vermasneha@users.noreply.github.com>
2024-01-16 12:45:17 +0530

    Update Minimum Moves to Equal Array Elements.cpp

--- Company 2/Minimum Moves to Equal Array Elements.cpp
+++ Company 2/Minimum Moves to Equal Array Elements.cpp
@@ -1,1 +1,16 @@
+class Solution {
+public:
+    int minMoves2(std::vector<int>& nums) {
+        // Find the median of the sorted nums array
+        std::sort(nums.begin(), nums.end());
+        int median = nums[nums.size() / 2];
 
+        // Calculate the sum of absolute differences between each element and the median
+        int moves = 0;
+        for (int num : nums) {
+            moves += std::abs(num - median);
+        }
+
+        return moves;
+    }
+};
