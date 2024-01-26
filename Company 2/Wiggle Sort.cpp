db4e9f2dd608329f286b1966d235a83c031653f7
Sneha kumari <90461178+Vermasneha@users.noreply.github.com>
2024-01-26 20:28:59 +0530

    Update Wiggle Sort.cpp

--- Company 2/Wiggle Sort.cpp
+++ Company 2/Wiggle Sort.cpp
@@ -1,1 +1,30 @@
+class Solution {
+public:
+    void wiggleSort(vector<int>& nums) {
+         ios_base::sync_with_stdio(false); 
+         cin.tie(NULL);    
+      
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        int right=n-1;
+        int left=n/2;
+        if(n%2==0){
+            left--;
+        }
+        vector<int> temp(n);
+        for(int i=0;i<n;i++){
+            if(i%2==0){
+                temp[i]=nums[left];
+                left--;
+            }
+            else{
+                  temp[i]=nums[right];
+                  right--;
+            }
+        }
+        nums=temp;
 
+
+        
+    }
+};
