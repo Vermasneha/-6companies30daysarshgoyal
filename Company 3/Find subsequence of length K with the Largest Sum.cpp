b08ab426862c673602575583d2ec8d7f55520b1d
Sneha kumari <90461178+Vermasneha@users.noreply.github.com>
2024-01-28 18:20:08 +0530

    Update Find subsequence of length K with the Largest Sum.cpp

--- Company 3/Find subsequence of length K with the Largest Sum.cpp
+++ Company 3/Find subsequence of length K with the Largest Sum.cpp
@@ -1,1 +1,17 @@
-
+class Solution {
+public:
+    vector<int> maxSubsequence(vector<int>& nums, int k) {
+        vector<int>temp = nums, ans; unordered_map<int,int>m;
+        sort(temp.begin(), temp.end(), greater<int>());
+        for(int i=0; i<k; i++){
+            m[temp[i]]++;
+        }
+        for(int &a:nums){
+            if(m.find(a) != m.end()) {
+                ans.push_back(a), m[a]--;
+                if(m[a] == 0) m.erase(a); 
+            }
+        }
+        return ans;
+    }
+};
