a64b8f1f2efa4908964b5857346ea6b5a36ed640
Sneha kumari <90461178+Vermasneha@users.noreply.github.com>
2024-01-29 12:02:05 +0530

    Update Top K Frequent Word.cpp

--- Company 3/Top K Frequent Word.cpp
+++ Company 3/Top K Frequent Word.cpp
@@ -1,1 +1,36 @@
+class Solution {
+public:
+    struct Compare {
+    bool operator()(const pair<int, string>& a, const pair<int, string>& b) {
+        if(a.first != b.first) return a.first < b.first;
+        return a.second > b.second;
+    }
+};
 
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        unordered_map<string,int> mp;            //string , frequency
+        int highest = 0;
+
+        for(auto word: words){
+            mp[word]++;
+            if(mp[word] >highest) highest = mp[word];
+        }
+
+        vector<string> ans;
+
+        //to pick the top most frequent strings
+        priority_queue<pair<int, string>, vector<pair<int, string>>, Compare> pq;
+
+        for(auto it: mp){
+            pq.push({it.second, it.first});
+        }
+
+        while(k){
+            ans.push_back(pq.top().second);
+            pq.pop();
+            k--;
+        }
+
+        return ans;
+    }
+};
