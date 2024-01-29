e36548fb2dfa02a6c96bdab990a650465044247d
Sneha kumari <90461178+Vermasneha@users.noreply.github.com>
2024-01-29 17:37:12 +0530

    Update Count Collisions on a Road.cpp

--- Company 5 (Atlassian)/Count Collisions on a Road.cpp
+++ Company 5 (Atlassian)/Count Collisions on a Road.cpp
@@ -1,1 +1,38 @@
+class Solution {
+public:
+    int countCollisions(string directions) {
+        int i, len = directions.length();
+        vector<char> stack;
 
+        int ans = 0;
+        for(i = len - 1; i >= 0; i--){
+            if(stack.empty() || directions[i] == 'L'){
+                stack.push_back(directions[i]);
+            }
+            else{
+                if(directions[i] == 'S'){
+                    while(!stack.empty() && stack.back() == 'L'){
+                        stack.pop_back();
+                        ans++;
+                    }
+                    stack.push_back('S');
+                }
+                else{
+                    if(stack.back() == 'S'){
+                        ans++;
+                    }
+                    else if(stack.back() == 'L'){
+                        ans += 2;
+                        stack.pop_back();
+                        while(!stack.empty() && stack.back() == 'L'){
+                            stack.pop_back();
+                            ans++;
+                        }
+                        stack.push_back('S');
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
