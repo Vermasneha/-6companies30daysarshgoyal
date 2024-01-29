61c4181435c622c39a08273ba9fe539b3c7fbf4d
Sneha kumari <90461178+Vermasneha@users.noreply.github.com>
2024-01-29 12:01:12 +0530

    Update Sort characaters by frequency.cpp

--- Company 3/Sort characaters by frequency.cpp
+++ Company 3/Sort characaters by frequency.cpp
@@ -1,1 +1,30 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+        string ans;
+        unordered_map<char,int> freq;
+        priority_queue<pair<int,char>> maxheap;
 
+        //storing freq of every chars
+        for(int i = 0; i<s.size(); i++){
+            freq[s[i]]++;
+        }
+
+
+        //stoting into heap
+        for(auto &pair: freq){
+            maxheap.push({pair.second,pair.first});
+        }
+
+        //final ans
+        while(!maxheap.empty()){
+            char c = maxheap.top().second;
+            int freq = maxheap.top().first;
+            while(freq--){
+                ans+=c;
+            }
+            maxheap.pop();
+        }
+        return ans;
+    }
+};
