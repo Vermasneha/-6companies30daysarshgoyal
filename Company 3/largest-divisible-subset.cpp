b1507855ad22373bb0b66c31b6947faff5850b7d
Sneha kumari <90461178+Vermasneha@users.noreply.github.com>
2024-01-29 12:05:08 +0530

    Update largest-divisible-subset.cpp

--- Company 3/largest-divisible-subset.cpp
+++ Company 3/largest-divisible-subset.cpp
@@ -1,1 +1,21 @@
-
+class Solution {
+public:
+    vector<int> largestDivisibleSubset(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n = nums.size();
+        vector<vector<int>> dp(n,vector<int>(0));
+        int mxSize=n-1;
+        for(int i = 0; i<n; i++) dp[i].push_back(nums[i]);
+        for(int i = n-1; i>=0; i--){
+            for(int j = i+1; j<n; j++){
+                if((nums[i]%nums[j]==0 || nums[j]%nums[i]==0) && dp[i].size()<dp[j].size()+1){
+                    dp[i].clear();
+                    dp[i].push_back(nums[i]);
+                    for(int &num : dp[j]) dp[i].push_back(num);
+                    if(dp[i].size()>dp[mxSize].size()) mxSize=i;
+                }
+            }
+        }
+        return dp[mxSize];
+    }
+};
