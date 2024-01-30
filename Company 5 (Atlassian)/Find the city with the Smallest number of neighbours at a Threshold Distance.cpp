04fa1ad39b6e1905f4a7529f89a53583facf1b76
Sneha kumari <90461178+Vermasneha@users.noreply.github.com>
2024-01-30 16:58:24 +0530

    Update Find the city with the Smallest number of neighbours at a Threshold Distance.cpp

--- Company 5 (Atlassian)/Find the city with the Smallest number of neighbours at a Threshold Distance.cpp
+++ Company 5 (Atlassian)/Find the city with the Smallest number of neighbours at a Threshold Distance.cpp
@@ -1,1 +1,43 @@
+class Solution {
+public:
+    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
 
+        vector<vector<int>> v(n,vector<int> (n,1e9));
+
+        for(auto it : edges){
+            v[it[0]][it[1]] = it[2];
+            v[it[1]][it[0]] = it[2];
+        }
+
+        for(int i = 0 ; i < n ; i++) v[i][i]=0;
+
+        for(int via = 0 ; via < n ; via++){
+            for(int i = 0 ; i < n ; i++){
+                for(int j = 0 ; j < n ; j++){
+                    v[i][j] = min(v[i][j] , v[i][via] + v[via][j]);
+                }
+            }
+        }
+
+        
+        int maxCnt = n;
+        int ans = -1;
+
+        for(int i = 0 ; i < n ; i++){
+            int cnt = 0;
+            for(int j = 0 ; j < n ; j++){
+                if(v[i][j] <= distanceThreshold){
+                    cnt++;
+                }
+            }
+            if(cnt<=maxCnt){
+                    maxCnt = cnt;
+                    ans = i;
+            }
+
+        }
+
+        return ans;
+
+    }
+};
