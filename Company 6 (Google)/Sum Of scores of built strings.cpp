3c5c1b30f9d8e43a05c9f7614bd7e03227a85c5d
Sneha kumari <90461178+Vermasneha@users.noreply.github.com>
2024-01-30 22:11:45 +0530

    Update Sum Of scores of built strings.cpp

--- Company 6 (Google)/Sum Of scores of built strings.cpp
+++ Company 6 (Google)/Sum Of scores of built strings.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
-    int stoneGameVI(vector<int>& a, vector<int>& b) {
-        int at=0,bt=0;
-        int n=a.size();
-        vector<pair<int,int>>v1;
-        for(int i=0;i<n;i++){
-            v1.push_back({a[i]+b[i],i});
+    vector<int> z_function(string &s)
+    {
+        int n=s.size();
+        vector<int> z(n);
+        for(int i=1,l=0,r=0;i<n;i++)
+        {
+            if(i<=r)
+                z[i]=min(r-i+1, z[i-l]);
+            while(i+z[i]<n && s[z[i]]==s[i+z[i]])
+                z[i]++;
+            if(i+z[i]-1>r)
+                l=i, r=i+z[i]-1;
         }
-        sort(v1.begin(),v1.end(),greater<pair<int,int>>());
-        for(int i=0;i<v1.size();i++){
-            if(i%2==0){
-                at+=a[v1[i].second];
-            }else bt+=b[v1[i].second];
-        }
-        
-        if(at>bt)return 1;
-        else if(at<bt)return -1;
-        else return 0;
+	return z;
+}
+    long long sumScores(string s) {
+        vector<int> z = z_function(s);
+        return max(accumulate(z.begin(), z.end(), 0LL), 0LL) + s.size();
     }
 };
