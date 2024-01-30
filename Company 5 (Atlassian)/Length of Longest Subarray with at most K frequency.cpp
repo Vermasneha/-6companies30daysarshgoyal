160497ad15972f8045f4f75a7eda34f5361d86e0
Sneha kumari <90461178+Vermasneha@users.noreply.github.com>
2024-01-30 21:35:00 +0530

    Update Length of Longest Subarray with at most K frequency.cpp

--- Company 5 (Atlassian)/Length of Longest Subarray with at most K frequency.cpp
+++ Company 5 (Atlassian)/Length of Longest Subarray with at most K frequency.cpp
@@ -1,1 +1,28 @@
-
+class Solution {
+public:
+    int maxSubarrayLength(vector<int>& nums, int k) {
+        int maxLength = 1;
+        int startIdx = 0;
+        unordered_map<int, int> frequency;
+        
+        for (int i = 0; i < nums.size(); ++i) {
+            if (frequency.find(nums[i]) == frequency.end()) {
+                frequency[nums[i]] = 1;
+            } else {
+                frequency[nums[i]] += 1;
+                
+                if (frequency[nums[i]] > k) {
+                    // Move the start index to the right until the count of the current number becomes <= k
+                    while (nums[startIdx] != nums[i]) {
+                        frequency[nums[startIdx]] -= 1;
+                        startIdx += 1;
+                    }
+                    startIdx += 1;
+                    frequency[nums[i]] -= 1;
+                }
+            }
+            maxLength = max(maxLength, i - startIdx + 1);
+        }
+        return maxLength;
+    }
+};
